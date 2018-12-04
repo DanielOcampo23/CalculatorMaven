db2f356167de47d021e38d3ebc575513c136dd64
sas1 <sas1@hgrid13.icesi.edu.co>
2018-12-04 11:56:35 -0500

    mejorado

--- src/main/java/CalculatorMaven/CalculatorMaven/Calculadora.java
+++ src/main/java/CalculatorMaven/CalculatorMaven/Calculadora.java
@@ -58,7 +58,23 @@ public class Calculadora {
 	}
 	
 	public boolean esPar(int a) {
-		return a%2==0;
+		boolena retorno=false;
+		String first=a+"";
+		char[] arreglo=first.toCharArray();
+		String last=arreglo[arreglo.length-1];
+		if(last=='0') {
+			retorno=true;
+		}else if(last=='2') {
+			retorno=true;
+		}else if(last=='4') {
+			retorno=true;
+		}else if(last=='6') {
+			retorno=true;
+		}else if(last=='8') {
+			retorno=true;
+		}
+		
+		return retorno;
 	}
 	
 	public boolean esImpar(int a) {
